ae143893b6834fddefae81130ec501d3b061d312
chmes <christine.meschede@hhu.de>
2013-12-15 09:19:59 +0100

    weitere prints gelöscht, damit es übersichtlicher ist

--- hole_daten.py
+++ hole_daten.py
@@ -12,30 +12,23 @@ mendeley = create_client()
 
 # aufgabe 1: Wie verteilen sich die in Mendeley abgelegten Publikationen auf die letzten 10 Jahre?
 # (Dafür müssen nicht alle Publikationen heruntergeladen werden!)
-print "Papers:\n"
 publikationen10 = {}
 for jahr in range(2003,2013): # letzte zehn jahre: von 2003 bis 2012 (trotzdem in range 2013 angeben)
     response = mendeley.search('year:'+str(jahr))
-    print str(jahr) + ":"
-    print "    "+str(response['total_results'])+" Publikationen"
     publikationen10[jahr]=response['total_results']
 #in json datei speichern
 with open("aufgabe1.json", "w") as json_output:
     json.dump(publikationen10, json_output)
-print "\n ---------------------- \n"
 
 
 # aufgabe 2: Was sind die Top20-Tags in der Kategorie „Computer and Information Science“?
 # hole die top 20 tags aus der kategorie "computer and information science"
 response = mendeley.tag_stats(6) # 6 ist die kategorie-id
-print "Top 20 Tags in Kategorie 'Computer and information science'\n"
 top20 = {}
 for tag in response:
     top20[tag['name']]=tag['count']
-    print tag['name'] +": "+ str(tag['count']) + " mal"
 with open("aufgabe2.json", "w") as json_output:
     json.dump(top20, json_output)
-print "\n ---------------------- \n"
 
 
 # aufgabe 3: Was sind die 10 populärsten (viele leser) Publikationen die in der Zeitschrift „Nature“ erschienen sind?
@@ -46,7 +39,6 @@ response = mendeley.search('published_in:Nature', items=100)
 print "Top 10 Publikationen in Zeitschrift 'Nature'\n"
 #pprint(response)
 print "anfrage muss noch verbessert werden"
-print "\n ---------------------- \n"
 
 
 # aufgabe 4: Auflistung aller Publikationen von Prof. Wolfgang G. Stock
@@ -56,12 +48,10 @@ print "\n ---------------------- \n"
 #       zusammengearbeitet hat nach Anzahl der in Mendeley vorhandenen,
 #       gemeinsamen Publikationen.
 response = mendeley.authored('"Wolfgang G Stock"', items=500)
-print "Publikationen von Stock\n"
 pub_jahre = {}
 coAutoren = {}
 for publikation in response['documents']:
     # Aufgabenteil a
-    print str(publikation['year']) + ": " + publikation['title'].encode('utf-8')
     if publikation['year'] in pub_jahre:
         pub_jahre[publikation['year']]+=1
     else:
@@ -69,9 +59,7 @@ for publikation in response['documents']:
     with open("aufgabe4a.json", "w") as json_output:
         json.dump(pub_jahre, json_output)
     #Aufgabenteil b
-    print "    Autoren:"
     for autor in publikation['authors']:
-        print "            " + autor['forename'] + " " + autor['surname']
         if autor['forename']!="Wolfgang G." and autor['surname']!="Stock":
             if autor['surname'] in coAutoren:
                 coAutoren[autor['surname']]+=1
@@ -79,12 +67,13 @@ for publikation in response['documents']:
                 coAutoren[autor['surname']]=1
     with open("aufgabe4b.json", "w") as json_output:
         json.dump(coAutoren, json_output)
-    print "\n"
-print "\n ---------------------- \n"
+
 
 
 # aufgabe 5: Suche nach dem Tag „ontology“ und bestimme die Häufigkeit für jede Kategorie in Mendeley für das Jahr 2011.
 # erster schritt: liste mit allen kategorien holen:
+total_pages = 1
+page = 0
 cat_response = mendeley.categories()
 ontology_anzahl={}
 for eintrag in cat_response: # jede kategorie durchgehen
